94d8fc49e2f1f2fcb7defdfd6ed3f96f75abf08f
Emkot87 <emkot87@live.com>
2022-12-20 20:22:34 +0200

    Update wide_add_krnl.cpp

--- wide_add_krnl.cpp
+++ wide_add_krnl.cpp
@@ -18,7 +18,7 @@ without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
 IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
 INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
 PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -95,51 +95,180 @@ extern "C"
 
     	for(int i = 0; i < m; i++){
     #pragma HLS loop_tripcount min=loop max=loop
+    		uint512_dt in2local = in2[i];
 
-    		uint512_dt in1local = in1[i];
-
+			#pragma HLS PIPELINE II=1
     		for(int j = 0; j < m; j++){
-				#pragma HLS PIPELINE II=1
-    			A_bram[i][j] = in1local.range(32*(j + 1) - 1, j*32);
+    			B_bram[j][i] = in2local.range(32*(j + 1) - 1, j*32);
     		}
     	}
 
 
     	for(int i = 0; i < m; i++){
-    #pragma HLS loop_tripcount min=loop max=loop
+    	    #pragma HLS loop_tripcount min=loop max=loop
 
-    	    uint512_dt in2local = in2[i];
+    	    		uint512_dt in1local = in1[i];
+    	    		uint512_dt resultV;
 
-    	    for(int k = 0; k< m ; k++){
-    	    	#pragma HLS PIPELINE II=1
-    	    	B_bram[k][j] = in2local.range(32*(k + 1) - 1, k*32);
-    	    }
-    	}
+    	    			for(int j = 0; j < m; j++){
+    						#pragma HLS PIPELINE II=1
+    	    				A_bram[i][j] = in1local.range(32*(j + 1) - 1, j*32);
+    	    			}
 
+    					for(int j = 0; j < m; j++){
+    						#pragma HLS loop_tripcount min=loop max=loop
 
+    						#pragma HLS PIPELINE II=1
+    						int result = 0;
+    						for(int k = 0 ; k < m ; k++){
+    							result += A_bram[i][k] * B_bram[k][j] ;
+    						}
 
-    	for(int i = 0 ; i < m ; i++){
-    		uint512_dt resultV;
-			for(int j = 0; j < m; j++){
-				#pragma HLS loop_tripcount min=loop max=loop
+    					resultV.range(32 * (j + 1) - 1, j * 32) = result;
 
-				#pragma HLS PIPELINE II=1
-				int result = 0;
-				for(int k = 0 ; k < m ; k++){
-					result += A_bram[i][k] * B_bram[k][j] ;
-				}
+    	    			}
 
-			resultV.range(32 * (j + 1) - 1, j * 32) = result;
-
-			}
-
-			out[i] = resultV;
+    				out[i] = resultV;
 
     	}
     }
 }
 
 
+
+
+
+///**********
+//Copyright (c) 2018, Xilinx, Inc.
+//All rights reserved.
+//
+//Redistribution and use in source and binary forms, with or without modification,
+//are permitted provided that the following conditions are met:
+//
+//1. Redistributions of source code must retain the above copyright notice,
+//this list of conditions and the following disclaimer.
+//
+//2. Redistributions in binary form must reproduce the above copyright notice,
+//this list of conditions and the following disclaimer in the documentation
+//and/or other materials provided with the distribution.
+//
+//3. Neither the name of the copyright holder nor the names of its contributors
+//may be used to endorse or promote products derived from this software
+//without specific prior written permission.
+//
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+//ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+//THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+//IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+//PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+//OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//**********/
+//
+///*******************************************************************************
+//Description:
+//    Wide Memory Access Example using ap_uint<Width> datatype
+//    Description: This is vector addition example to demonstrate Wide Memory
+//    access of 512bit Datawidth using ap_uint<> datatype which is defined inside
+//    'ap_int.h' file.
+//*******************************************************************************/
+//
+////Including to use ap_uint<> datatype
+//#include <ap_int.h>
+//#include <stdio.h>
+//#include <string.h>
+//
+//#define BUFFER_SIZE 64
+//#define DATAWIDTH 512
+//#define VECTOR_SIZE (DATAWIDTH / 32) // vector size is 16 (512/32 = 16)
+//typedef ap_uint<DATAWIDTH> uint512_dt;
+//
+////TRIPCOUNT identifier
+//const unsigned int c_chunk_sz = BUFFER_SIZE;
+//const unsigned int c_size     = VECTOR_SIZE;
+//
+//#define lm 4
+//#define ln 4
+//#define lp 4
+//
+//#define m  1<<lm
+//#define n  1<<ln
+//#define p  1<<lp
+//
+///*
+//    Vector Addition Kernel Implementation using uint512_dt datatype
+//    Arguments:
+//        in1   (input)     --> Input Vector1
+//        in2   (input)     --> Input Vector2
+//        out   (output)    --> Output Vector
+//        size  (input)     --> Size of Vector in Integer
+//   */
+//extern "C"
+//{
+//    void wide_vadd(
+//        const uint512_dt *in1, // Read-Only Vector 1
+//        const uint512_dt *in2, // Read-Only Vector 2
+//        uint512_dt *out,       // Output Result
+//        int size               // Size in integer
+//    )
+//    {
+//#pragma HLS INTERFACE m_axi port = in1 max_write_burst_length = 32 max_read_burst_length = 32 offset = slave bundle = gmem
+//#pragma HLS INTERFACE m_axi port = in2 max_read_burst_length = 32 offset = slave bundle = gmem1
+//#pragma HLS INTERFACE m_axi port = out max_write_burst_length = 32 max_read_burst_length = 32 offset = slave bundle = gmem2
+//#pragma HLS INTERFACE s_axilite port = in1 bundle = control
+//#pragma HLS INTERFACE s_axilite port = in2 bundle = control
+//#pragma HLS INTERFACE s_axilite port = out bundle = control
+//#pragma HLS INTERFACE s_axilite port = size bundle = control
+//#pragma HLS INTERFACE s_axilite port = return bundle = control
+//
+//
+//    	const int loop = m;
+//
+//    	int A_bram[n][m];
+//    	int B_bram[p][n];
+//
+//    #pragma HLS ARRAY_PARTITION variable=A_bram cyclic factor 8 dim=2
+//    #pragma HLS ARRAY_PARTITION variable=B_bram cyclic factor 8 dim=1
+//
+//
+//    	for(int i = 0; i < m; i++){
+//    #pragma HLS loop_tripcount min=loop max=loop
+//
+//    		uint512_dt in1local = in1[i];
+//    		uint512_dt in2local = in2[i];
+//
+//			#pragma HLS PIPELINE II=1
+//    		for(int j = 0; j < m; j++){
+//    			A_bram[i][j] = in1local.range(32*(j + 1) - 1, j*32);
+//    			B_bram[j][i] = in2local.range(32*(j + 1) - 1, j*32);
+//    		}
+//    	}
+//
+//
+//    	for(int i = 0 ; i < m ; i++){
+//    		uint512_dt resultV;
+//			for(int j = 0; j < m; j++){
+//				#pragma HLS loop_tripcount min=loop max=loop
+//
+//				#pragma HLS PIPELINE II=1
+//				int result = 0;
+//				for(int k = 0 ; k < m ; k++){
+//					result += A_bram[i][k] * B_bram[k][j] ;
+//				}
+//
+//				resultV.range(32 * (j + 1) - 1, j * 32) = result;
+//
+//				}
+//
+//			out[i] = resultV;
+//
+//    	}
+//    }
+//}
+//
+//
 
 
 
